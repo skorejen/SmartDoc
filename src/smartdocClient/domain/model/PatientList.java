344b25aad2f29fa4a4907623351edb9ea7cf9ab0
unknown <mihai.tirtara@gmail.com>
2018-05-18 15:41:44 +0200

    Addet in Model Patient and PatientList

---
+++ src/smartdocClient/domain/model/PatientList.java
@@ -0,0 +1,86 @@
+package smartdocClient.domain.model;
+
+import java.util.ArrayList;
+
+public class PatientList 
+{
+	private ArrayList<Patient> patients;
+
+	public PatientList() 
+	{
+		patients = new ArrayList<>();
+	}
+	
+	public int getNumberOfPatients()
+	{
+		return patients.size();
+	}
+	
+	public void addPatient(Patient p)
+	{
+		patients.add(p);
+	}
+	
+	public Patient getPatient(int index)
+	{
+		return patients.get(index);
+	}
+	
+	public void removePatient(int index)
+	{
+		patients.remove(index);
+	}
+	
+	public boolean isEmpty()
+	{
+		if(patients.size() == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public Patient getPatientByFname(String fname)
+	{
+		for(int i = 0;i<patients.size();i++)
+		{
+			if(patients.get(i).getFname() == fname)
+			{
+				return patients.get(i);
+			}
+		}
+		
+		return null;
+	}
+	
+	public Patient getPatientByLname(String lname)
+	{
+		for(int i = 0;i<patients.size();i++)
+		{
+			if(patients.get(i).getLname() == lname)
+			{
+				return patients.get(i);
+			}
+		}
+		
+		return null;
+	}
+	
+	public Patient getPatientByCpr(String cpr)
+	{
+		for(int i = 0;i<patients.size();i++)
+		{
+			if(patients.get(i).getCpr() == cpr)
+			{
+				return patients.get(i);
+			}
+		}
+		
+		return null;
+	}
+	
+	}
+
