73d604fd44a7a9ddaa774f29706ce2f5b12c8f36
masterdisk <32034582+masterdisk@users.noreply.github.com>
2018-05-19 16:57:03 +0200

    Doctor 1.3.5

---
+++ src/smartdocClient/controller/DoctorLoggedInController.java
@@ -0,0 +1,60 @@
+package smartdocClient.controller;
+
+import java.awt.Button;
+import java.awt.Label;
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class DoctorLoggedInController {
+
+	@FXML
+	private Button goToMyPacients;
+	@FXML
+	private Button myInfo;
+	@FXML
+	private Button signOut;
+	@FXML
+	private Label doctorName;
+	
+	public void goToMyPacientsButtonPressed(ActionEvent event) throws IOException {
+		{
+			Parent register = FXMLLoader.load(getClass().getResource("../view/SUCCESS.fxml"));
+			Scene home_page_scene = new Scene(register);
+			Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			app_stage.setScene(home_page_scene);
+			app_stage.show();
+		}
+
+	}
+	
+	public void myInfoButtonPressed(ActionEvent event) throws IOException {
+		{
+			Parent register = FXMLLoader.load(getClass().getResource("../view/MyInfoDoctor.fxml"));
+			Scene home_page_scene = new Scene(register);
+			Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			app_stage.setScene(home_page_scene);
+			app_stage.show();
+		}
+
+	}
+	
+	public void signOutButtonPressed(ActionEvent event) throws IOException {
+		{
+			Parent register = FXMLLoader.load(getClass().getResource("../view/LoginPage.fxml"));
+			Scene home_page_scene = new Scene(register);
+			Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			app_stage.setScene(home_page_scene);
+			app_stage.show();
+		}
+
+	}
+	
+	
+}
