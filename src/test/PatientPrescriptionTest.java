7788bb657cd7dc5ac5ccc30782b56d20f9684f1c
266100 <266100@LU52-18231-ITUD.VIA.DK>
2018-05-31 18:28:50 +0200

    Test Prescription

---
+++ src/test/PatientPrescriptionTest.java
@@ -0,0 +1,71 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import smartdocServer.domain.model.PatientPrescription;
+import smartdocServer.domain.mediator.ServerModelManager;
+
+class PatientPrescriptionTest {
+
+		static PatientPrescription patientPrescription;
+		static LocalDate date = LocalDate.now();
+		static ServerModelManager server;
+	
+	@BeforeAll
+	
+	public static void initialize() {
+		server = ServerModelManager.getInstance();
+		patientPrescription= new PatientPrescription("070999-1111", "Medicine", date, "Headache", "Sleep");
+	}
+	@Test //Works
+	void testCpr() {
+		patientPrescription.setCpr("070999-1111");
+		patientPrescription.getCpr();
+		assertEquals(patientPrescription.getCpr(),"070999-1111");
+
+		
+	}
+	
+	@Test //Works
+	void testPrescription() {
+		patientPrescription.setPrescription("Medicine");
+		patientPrescription.getPrescription();
+		assertEquals(patientPrescription.getPrescription(),"Medicine");
+
+		
+	}
+
+	@Test //Works
+	void testAppointments() {
+		patientPrescription.setAppointments(date);
+		patientPrescription.getAppointments();
+		assertEquals(patientPrescription.getAppointments(),date);
+
+		
+	}
+	
+	@Test //Works
+	void testProblem() {
+		patientPrescription.setProblem("Headache");
+		patientPrescription.getProblem();
+		assertEquals(patientPrescription.getProblem(),"Headache");
+
+		
+	}
+	
+	@Test //Works
+	void testRecommendation() {
+		patientPrescription.setRecommendation("Sleep");
+		patientPrescription.getRecommendation();
+		assertEquals(patientPrescription.getRecommendation(),"Sleep");
+
+		
+	}
+
+}
