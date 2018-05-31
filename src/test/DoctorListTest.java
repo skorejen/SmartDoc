d6a6b88f5b616d23bc526f8ebd54bcb042dd4499
266100 <266100@LU52-18231-ITUD.VIA.DK>
2018-05-31 17:37:23 +0200

    Final Doctor-PatientList

--- src/test/DoctorListTest.java
+++ src/test/DoctorListTest.java
@@ -4,67 +4,99 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import smartdocClient.domain.model.Doctor;
-import smartdocClient.domain.model.DoctorList;
+import smartdocServer.domain.model.Doctor;
+import smartdocServer.domain.model.DoctorList;
+import smartdocServer.domain.mediator.ServerModelManager;
+import smartdocServer.domain.model.Patient;
+import smartdocServer.domain.model.PatientList;
 
 class DoctorListTest {
+	static DoctorList doctorList;
+	static ServerModelManager server;
+	static LocalDate date = LocalDate.now();
+	static Doctor doctor1;
+	static Doctor doctor2;
+	
+	@BeforeAll
+		public static void initialize(){
+			server = ServerModelManager.getInstance();
+			doctorList = new DoctorList();
+			LocalDate date = LocalDate.now();
+			doctor1 = new Doctor("070999-1100", "Juan", "Pastor", date, 232323, "amigo@via.com", "d",
+					"f", "cardiologist");
+			doctor2 = new Doctor("070999-1111", "Jose", "Pastor", date, 232323, "amigo@via.com", "d",
+					"m", "cardiologist");
+			}
+	
 
-	@Test
-	void test() {
 		
 
-		LocalDate date = LocalDate.now();
-		Doctor doctor1 = new Doctor("070999-1100", "Juan", "Pastor", date, 232323, "amigo@via.com", "d",
-				"f", "cardiologist");
-		Doctor doctor2 = new Doctor("070999-1111", "Jose", "Pastor", date, 232323, "amigo@via.com", "d",
-				"m", "cardiologist");
+	
+	@Test //works
 		
-	//////Test DoctorList: addDoctor, getNumberOfDoctors
-		DoctorList doctorList = new DoctorList();
+	void testAddDoctorGetNumberOfDoctors() {
 		doctorList.addDoctor(doctor1);
 		doctorList.addDoctor(doctor2);
-		
-		assertEquals(doctorList.getNumberOfDoctors(),2);
-
-	//////Test DoctorList: getDoctor
-
-		
-		assertEquals(doctorList.getDoctor(0), doctor1);
+		assertEquals(doctorList.getNumberOfDoctors(),3);
+		}
 	
-				
-	//////Test DoctorList: removeDoctor
+
+
+
+		
+		
 		
 
-		doctorList.removeDoctor(0);
+	@Test //Works
+		void testGetDoctor() {
+
 		assertEquals(doctorList.getDoctor(0), doctor2);
+		}
+
 
 			
-	//////Test DoctorList: isEmpty
+	@Test //Works
+	 	void testRemoveDoctor() {
+		
+
+		doctorList.addDoctor(doctor1);
+		doctorList.addDoctor(doctor2);
+		doctorList.removeDoctor(0);
+		assertEquals(doctorList.getDoctor(0), doctor2);
+		}
+
+	@Test //Works
+	void testIsEmpty() {
 
 		//doctorList.removeDoctor(0);
 		assertEquals(doctorList.isEmpty(), false);
-		
+	}
 
-		
-	//////Test DoctorList: getDoctorByFname
+	
+	@Test //Works
+	void TestGetDoctorByFname() {
 
 		assertEquals(doctorList.getDoctorByFname("Jose"), doctor2);
-		
-	//////Test DoctorList: getDoctorByLname
+	}
+	@Test //Works
+	void testGetDoctorByLname(){
 
 		assertEquals(doctorList.getDoctorByLname("Pastor"), doctor2);
-		
+	}
 
-	//////Test DoctorList: getDoctorByCpr
+	@Test //Works
+	void TestGetDoctorByCpr(){
 
 		assertEquals(doctorList.getDoctorByCpr("070999-1111"), doctor2);		
-
-	//////Test DoctorList: getDoctorBySpeciality
+	}
+	@Test //Works
+	void TestGetDoctorBySpeciality(){
 
 			assertEquals(doctorList.getSpeciality("cardiologist"), doctor2);		
 			
 	}
+	
 }
-
--- src/test/PatientListTest.java
+++ src/test/PatientListTest.java
@@ -2,64 +2,106 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
-import smartdocClient.domain.model.Patient;
-import smartdocClient.domain.model.PatientList;
+import smartdocServer.domain.model.Patient;
+import smartdocServer.domain.model.PatientList;
+import smartdocServer.domain.mediator.ServerModelManager;
 
 class PatientListTest {
 
-	@Test
-	void test() {
-	//	fail("Not yet implemented");
+	static PatientList patientList;
+	static ServerModelManager server;
+	static LocalDate date = LocalDate.now();
+	static Patient patient1;
+	static Patient patient2;
+	static Patient patient3;
+	
+	@BeforeAll
+		public static void initialize(){
+			server = ServerModelManager.getInstance();
+			patientList = new PatientList();
+			patient1 = new Patient("070999-0000", "Pedro", "Claro", date, 11111111, "claro@gmail.com", "p",
+					"m");
+			patient2 = new Patient("070999-0010", "Amaia", "Sanchez", date, 22222222, "pedrosanchez@gmail.com", "p",
+					"f");
+			patient3 = new Patient("122999-0010", "Lunes", "Pastor", date, 33333333, "lunes@gmail.com", "p",
+					"m");
+		}
+		
+		
+	/*static LocalDate date = LocalDate.now();
+	static Patient patient1 = new Patient("070999-0000", "Pedro", "Sanchez", date, 11111111, "pedrosanch@gmail.com", "p",
+		"m");
+	static Patient patient2 = new Patient("070999-0000", "Amaia", "Sanchez", date, 11111111, "pedrosanchez@gmail.com", "p",
+			"m");*/
 		
 
-		LocalDate date = LocalDate.now();
-		Patient patient1 = new Patient("070999-0000", "Pedro", "Sanchez", date, 11111111, "pedrosanchez@gmail.com", "p",
-			"m");
-		Patient patient2 = new Patient("070999-0000", "Amaia", "Sanchez", date, 11111111, "pedrosanchez@gmail.com", "p",
-				"m");
-	//////Test PatientList: addPatient, getNumberOfPatients
-		PatientList patientList = new PatientList();
+		@Test //works
+
+		void testAddPatientGetNumberOfPatients() {
 		patientList.addPatient(patient1);
 		patientList.addPatient(patient2);
 		
-		assertEquals(patientList.getNumberOfPatients(),2);
-	//////Test PatientList: getPatient
+		assertEquals(patientList.getNumberOfPatients(),3);
+		}
+		
+		@Test //works
 
+
+		void testGetPatient() {
+		assertEquals(patientList.getPatient(0), patient2);
+		}
 		
-		assertEquals(patientList.getPatient(0), patient1);
-		
-	//////Test PatientList: removePatient
+		@Test //works
 		
 
+		void testRemovePatient() {
+			patientList.addPatient(patient1);
+			patientList.addPatient(patient2);
 		patientList.removePatient(0);
 		assertEquals(patientList.getPatient(0), patient2);
+		}
+
 		
-	//////Test PatientList: isEmpty
-		//patientList.removePatient(0);
+		@Test //works
+
+		void testIsEmpty() {
+			//patientList.removePatient(0);
 		assertEquals(patientList.isEmpty(), false);
-		
-	//////Test PatientList: getPatientByFname
+		}
 
-		assertEquals(patientList.getPatientByFname("Amaia"), patient2);
 		
-		
-	//////Test PatientList: getPatientByLname
+		@Test //works
 
-		assertEquals(patientList.getPatientByLname("Sanchez"), patient2);
+		void testGetPatientByFname() {
+			patient2=patientList.getPatientByFname("Amaia");
+		assertEquals(patient2, this.patient2);
+		}
 		
-		
-	//////Test PatientList: getPatientByCpr
+		@Test //works
 
-		assertEquals(patientList.getPatientByCpr("070999-0000"), patient2);		
+		void testPatientList() {
+
+			patient2=patientList.getPatientByLname("Sanchez");
+		assertEquals(patient2, this.patient2);
+		}
+		
+		@Test //works
+
+		void testGetPatientByCpr() {
+			patient2=patientList.getPatientByCpr("070999-0010");
+		assertEquals(patient2, this.patient2);		
 		
 		
 		
 		
 
 	}
+		
 
 }
 
